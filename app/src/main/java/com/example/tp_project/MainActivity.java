dc6843ecf14bc9d4ff7a65815f3191a2e1cf18ae
FEL <Vovafelinger75@gmail.com>
2023-04-14 11:51:38 +0700

    добавил авторизацию, сообщение при успешной регистрации/авторизации, и чтобы всплывающее окно с формой открывалось заново если регистрация/авторизация прошла не успешно(при неуспешной авторизации, сохраняется введённый логин)

--- app/src/main/java/com/example/tp_project/MainActivity.java
+++ app/src/main/java/com/example/tp_project/MainActivity.java
@@ -23,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     Button btn_regist;
+    Button btn_login;
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputName;
     private TextInputLayout textInputPassword;
@@ -35,6 +36,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         btn_regist = findViewById(R.id.btn_regist);
+        btn_login = findViewById(R.id.btn_login);
 
         btn_regist.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,7 +45,75 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {showLoginWindow("");
+
+            }
+        });
+
     }
+
+    private void showLoginWindow(String mail) {
+        AlertDialog.Builder dialog_login = new AlertDialog.Builder(this);
+        dialog_login.setTitle("Вход")
+                .setMessage("Введите все данные");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View aut_window = inflater.inflate(R.layout.aut_window, null);
+        dialog_login.setView(aut_window);
+
+        textInputEmail = aut_window.findViewById(R.id.textInputEmail);
+        textInputPassword = aut_window.findViewById(R.id.textInputPassword);
+
+        if (!mail.isEmpty())  {
+            textInputEmail.getEditText().setText(mail);
+        }
+
+        dialog_login.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email = textInputEmail.getEditText().getText().toString().trim();
+                String password = textInputPassword.getEditText().getText().toString().trim();
+                loginUser(email, password);
+            }
+
+            })
+                .setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+        AlertDialog alert = dialog_login.create();
+        alert.show();
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("login", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "Authentication succesed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            showLoginWindow(email); //если авторизация не удалась, заново открывается окно
+
+                        }
+                    }
+                });
+    }
+
 
     private void showRegisterWindow() {
         AlertDialog.Builder dialog_register = new AlertDialog.Builder(this);
@@ -86,6 +156,8 @@ public class MainActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d("signup", "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Authentication succesed.",
+                                    Toast.LENGTH_SHORT).show();
                             FirebaseUser user = mAuth.getCurrentUser();
 
                             //updateUI(user);
@@ -94,6 +166,7 @@ public class MainActivity extends AppCompatActivity {
                             Log.w("signup", "createUserWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
+                            showRegisterWindow(); //если регистрация не удалась, заново открывается окно
                             //updateUI(null);
                         }
                     }
