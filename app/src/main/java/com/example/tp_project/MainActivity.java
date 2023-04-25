08b13ac5fa8d1fbec35c1d66abe566240b1c515a
IrenPsheno <115718957+iren221@users.noreply.github.com>
2023-04-25 12:14:35 +0700

    Начата регистрация животного(фото)Ð

---
+++ app/src/main/java/com/example/tp_project/MainActivity.java
@@ -0,0 +1,209 @@
+package com.example.tp_project;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    Button btn_regist;
+    Button btn_login;
+    private TextInputLayout textInputEmail;
+    private TextInputLayout textInputName;
+    private TextInputLayout textInputPassword;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+    DatabaseReference myRef = database.getReference("test");
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        btn_regist = findViewById(R.id.button2);
+        btn_login = findViewById(R.id.button1);
+
+        btn_regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRegisterWindow();
+            }
+        });
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {showLoginWindow("");
+
+            }
+        });
+
+    }
+
+    private void showLoginWindow(String mail) {
+        AlertDialog.Builder dialog_login = new AlertDialog.Builder(this);
+        dialog_login.setTitle("Вход")
+                .setMessage("Введите все данные");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View aut_window = inflater.inflate(R.layout.aut_window, null);
+        dialog_login.setView(aut_window);
+
+        textInputEmail = aut_window.findViewById(R.id.textInputEmail);
+        textInputPassword = aut_window.findViewById(R.id.textInputPassword);
+
+        if (!mail.isEmpty())  {
+            textInputEmail.getEditText().setText(mail);
+        }
+
+        dialog_login.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email = textInputEmail.getEditText().getText().toString().trim();
+                String password = textInputPassword.getEditText().getText().toString().trim();
+                loginUser(email, password);
+
+            }
+
+            })
+                .setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.dismiss();
+                    }
+                });
+
+        AlertDialog alert = dialog_login.create();
+        alert.show();
+    }
+
+    private void register_petActivity() {
+        Intent register_pet = new Intent(this, Register_pet.class);
+        startActivity(register_pet);
+    }
+
+    private void loginUser(String email, String password) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("login", "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            myRef.child("users").child(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() { // функция считывающая данные из бд
+                                @Override
+                                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                    if (!task.isSuccessful()) {
+                                        Toast.makeText(MainActivity.this, "error",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+
+                                        String s = task.getResult().child("name").getValue().toString(); //получаем имя пользователя из бд
+                                        Toast.makeText(MainActivity.this, "hello "+s,
+                                                Toast.LENGTH_SHORT).show();
+                                        register_petActivity();
+                                    }
+                                }
+                            });
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("login", "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            showLoginWindow(email); //если авторизация не удалась, заново открывается окно
+
+                        }
+                    }
+                });
+    }
+
+
+    private void showRegisterWindow() {
+        AlertDialog.Builder dialog_register = new AlertDialog.Builder(this);
+        dialog_register.setTitle("Регистрация")
+                .setMessage("Введите все данные");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View register_window = inflater.inflate(R.layout.register_window, null);
+        dialog_register.setView(register_window);
+
+        textInputEmail = register_window.findViewById(R.id.textInputEmail);
+        textInputName = register_window.findViewById(R.id.textInputName);
+        textInputPassword = register_window.findViewById(R.id.textInputPassword);
+
+        dialog_register.setPositiveButton("Добавить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                String email = textInputEmail.getEditText().getText().toString().trim();
+                String password = textInputPassword.getEditText().getText().toString().trim();
+                String name = textInputName.getEditText().getText().toString().trim();
+                addUser(email, password, name);
+            }
+        })
+
+        .setNegativeButton("Отменить", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        AlertDialog alert = dialog_register.create();
+        alert.show();
+    }
+
+    private void addUser(String email, String password, String name) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("signup", "createUserWithEmail:success");
+                            Toast.makeText(MainActivity.this, "Registration succesed.",
+                                    Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            myRef.child("users").child(user.getUid()).child("name").setValue(name); // добавляем в бд имя пользователя
+
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("signup", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Registration failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            showRegisterWindow(); //если регистрация не удалась, заново открывается окно
+                            //updateUI(null);
+                        }
+                    }
+                });
+    }
+}
---
+++ app/src/main/java/com/example/tp_project/Register_pet.java
@@ -0,0 +1,147 @@
+package com.example.tp_project;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class Register_pet extends AppCompatActivity {
+
+    Button btn_download_photo_pet, btn_ok;
+
+    ImageView download_photo_pet;
+    EditText pet_name, pet_age, pet_weight;
+    FirebaseStorage storage;
+    StorageReference storageRef;
+    private Uri upload_Uri;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_pet);
+
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        //storageRef = FirebaseStorage.getInstance().getReference("ImagePet");
+        btn_download_photo_pet =  findViewById(R.id.btn_download_photo_pet);
+        download_photo_pet = findViewById(R.id.download_photo_pet);
+        pet_name = findViewById(R.id.pet_name);
+        pet_age = findViewById(R.id.age_pet);
+        pet_weight = findViewById(R.id.weight_pet);
+
+
+        btn_download_photo_pet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getImage();
+            }
+        });
+
+//        btn_ok.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//                if (!pet_name.getText().toString().isEmpty() && !pet_age.getText().toString().isEmpty() && !pet_weight.getText().toString().isEmpty()) {
+//                    Toast.makeText(getApplicationContext(), "Good", Toast.LENGTH_SHORT).show();
+//
+//                }else {
+//                    Toast.makeText(getApplicationContext(), "Error", Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+    }
+
+
+
+
+    private void getImage() {
+        Intent intentChooser = new Intent();
+        intentChooser.setType("image/*");
+        intentChooser.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intentChooser, "Select your image free here..."), 1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && data != null && data.getData() != null) {
+            if (resultCode == RESULT_OK) {
+                download_photo_pet.setImageURI(data.getData());
+                uploadImage();
+                //upload_Uri = data.getData();
+
+//                try {
+//                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), upload_Uri);
+//                    download_photo_pet.setImageBitmap(bitmap);
+//                }catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+                //uploadImage();
+            }
+        }
+    }
+    private void uploadImage() {
+        Bitmap bitmap = ((BitmapDrawable) download_photo_pet.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] byteArray = baos.toByteArray();
+        final StorageReference myRef = storageRef.child(System.currentTimeMillis() + "pet_image");
+        UploadTask up = myRef.putBytes(byteArray);
+        Task<Uri> task = up.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                return myRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                upload_Uri = task.getResult();
+                Toast.makeText(Register_pet.this, "Картинка загружена в хранилище",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+//        if (upload_Uri != null) {
+//            ProgressDialog progressDialog = new ProgressDialog(Register_pet.this);
+//            progressDialog.setTitle("Загрузка картинки");
+//            progressDialog.show();
+//
+//            String imageName = Double.toString(Math.random());
+//            storageRef.child("image").child(imageName).putFile(upload_Uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//                @Override
+//                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//                    progressDialog.dismiss();
+//                    Toast.makeText(getApplicationContext(), "Image upload", Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        }
+
+    }
+}
