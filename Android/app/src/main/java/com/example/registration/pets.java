3d81ce185151cde7679c1cf5725271a91ba04203
zewa-bamuga <tikhonov.igor2028@yandex.ru>
2023-05-15 18:10:29 +0700

    Все экраны

---
+++ Android/app/src/main/java/com/example/registration/pets.java
@@ -0,0 +1,14 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pets extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets);
+    }
+}
---
+++ Android/app/src/main/java/com/example/registration/setting_up_notifications.java
@@ -0,0 +1,14 @@
+package com.example.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class setting_up_notifications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_up_notifications);
+    }
+}
